c7b487bdfe5a889747a5d35f52b507801d114f44
Susmitha510 <susmi1005@gmail.com>
2026-06-17 12:31:21 +0530

    Fix 1

--- server/routes/resetPassword.js
+++ server/routes/resetPassword.js
@@ -9,13 +9,7 @@ const bcrypt = require('bcryptjs');
 // Email Transporter
 // ======================
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
-  family: 4,
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
+service: 'gmail',
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD
